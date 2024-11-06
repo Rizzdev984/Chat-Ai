333d62627b71a47fc024d9b73aa71c9dc6a32864
Rizzdev984 <anjai1233211@gmail.com>
2024-11-06 17:33:22 +0700

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatWindow from '../components/ChatWindow';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Chat with AI</h1>
+            <ChatWindow />
+        </div>
+    );
+};
+
+export default HomePage;
