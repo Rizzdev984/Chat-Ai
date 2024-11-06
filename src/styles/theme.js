3075a17d52939cfcb6cacb562a43a96d6a570656
Rizzdev984 <anjai1233211@gmail.com>
2024-11-06 17:39:44 +0700

    Create theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+    primaryColor: '#007bff',
+    secondaryColor: '#6c757d',
+    backgroundColor: '#f4f4f4',
+    textColor: '#212529',
+    hoverColor: '#0056b3',
+};
+
+export default theme;
